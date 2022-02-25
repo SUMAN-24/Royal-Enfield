6f0318cd62b56f4103f1e5068c524ac4614ac4b1
Gaurav Kumar <gauravkr2179@gmail.com>
2022-02-25 18:31:13 +0530

    update

---
+++ helmetData.js
@@ -0,0 +1,110 @@
+var helmetData = [
+    {
+      name: "ESCAPADE HELMET NS - MATT BLACK",
+      image_url: "https://store.royalenfield.com/media/catalog/product/0/r/0r6a5877-edit.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "3200.00",
+      size:{
+        M: 0,
+        L: 10,
+        XL: 10,
+        One_Size: 10
+
+      }
+    },
+    {
+      name: "SUNDOWN HELMET - BATTLE GREEN",
+      image_url: "https://store.royalenfield.com/media/catalog/product/0/r/0r6a6304-edit-2.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "2700",
+      size:{
+        M: 10,
+        L: 0,
+        XL: 10,
+        One_Size: 5
+      }
+    },
+    {
+      name: "SUNDOWN HELMET - DESERT STORM",
+      image_url: "https://store.royalenfield.com/media/catalog/product/0/r/0r6a6304-edit.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "2700"
+    },
+    {
+      name: "SUNDOWN HELMET - SQ BLUE",
+      image_url: "https://store.royalenfield.com/media/catalog/product/0/r/0r6a5906-edit.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "2700"
+    },
+    {
+      name: "ESCAPADE HELMET NS - DESERT STORM",
+      image_url: "https://store.royalenfield.com/media/catalog/product/0/j/0j1a9923-edit.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "3200"
+    },
+    {
+      name: "ESCAPADE HELMET NS - MATT BLACK",
+      image_url: "https://store.royalenfield.com/media/catalog/product/0/r/0r6a5877-edit.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "3200"
+    },
+    {
+      name: "SUNDOWN HELMET - SQ BLUE",
+      image_url: "https://store.royalenfield.com/media/catalog/product/0/r/0r6a5906-edit.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "2700"
+    },
+    {
+      name: "MODULAR ADROIT HELMET - SQ BLUE",
+      image_url: "https://store.royalenfield.com/media/catalog/product/0/r/0r6a5950-edit.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "2500"
+    },
+    {
+      name: "SUNDOWN HELMET - GLOSS WHITE",
+      image_url: "https://store.royalenfield.com/media/catalog/product/0/r/0r6a6230-edit.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "2500"
+    },
+    {
+      name: "MODULAR ADROIT HELMET - GLOSS WHITE",
+      image_url: "https://store.royalenfield.com/media/catalog/product/0/j/0j1a0020-edit.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "2500"
+    },
+    {
+      name: "QUEST HELMET - SQ BLUE",
+      image_url: "https://store.royalenfield.com/media/catalog/product/h/e/heaw21006mattsquadronblue_1.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "2300"
+    },
+    {
+      name: " STREET PRIME CRACKLING CAMO HELMET-CHARCOAL",
+      image_url: "https://store.royalenfield.com/media/catalog/product/r/e/re-size-7.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "3700"
+    },
+    {
+      name: "STREET PRIME BOLT HELMET-WHITE & YELLOW",
+      image_url: "https://store.royalenfield.com/media/catalog/product/r/e/re-size-8_1.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "3700"
+    },
+    {
+      name: "CLASSIC CAMO PRINT HELMET-BATTLE GREEN",
+      image_url: "https://store.royalenfield.com/media/catalog/product/r/e/rehel_00239_1.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "2900"
+    },
+    {
+      name: "CLASSIC RIDE MORE HELMET-GLOSS BLACK",
+      image_url: "https://store.royalenfield.com/media/catalog/product/r/e/rehel_00183_1.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "2700"
+    },
+    {
+      name: "TWIN STRIPES SUNPEAK HELMET-GLOSS WHITE",
+      image_url: "https://store.royalenfield.com/media/catalog/product/2/0/20006.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "1750"
+    },
+    {
+      name: "NH44-GLOSS RED ECE & ISI certified",
+      image_url: "https://store.royalenfield.com/media/catalog/product/r/e/re-size-21.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "4800"
+    },
+    {
+      name: "SPIRIT GLOSS WHITE & RED",
+      image_url: "https://store.royalenfield.com/media/catalog/product/h/e/helmets-heaw19010-gloss_white-4.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "3600"
+    },
+    {
+      name: "JET OPEN FACE MLG HELMET-MATT DESERT STORM",
+      image_url: "https://store.royalenfield.com/media/catalog/product/2/1/215a0524-edit.jpg?width=275&height=275&canvas=275:275&quality=80&bg-color=255,255,255",
+      price: "1750"
+    }
+  ]
