16962aa9a26623e6311cf488c456821013160a4f
Abinash Panigrahi <abinash24x7@gmail.com>
2022-02-26 17:41:35 +0530

    added login, forgot_pwd page

---
+++ Scripts/login.js
@@ -0,0 +1,52 @@
+var regdUsers=JSON.parse(localStorage.getItem("signUpdataBase")) || [];
+
+console.log(regdUsers);
+
+var form= document.querySelector("#form");
+
+document.querySelector("#form").addEventListener("submit", formSubmit);
+  
+function formSubmit(event) {
+    event.preventDefault();
+
+    var user = form.userId.value;
+    var pass = form.password.value;
+    var check = false;
+    
+    for (var i = 0; i < regdUsers.length; i++) {
+        if ((regdUsers[i].Email== user || regdUsers[i].Mobile==user ) && regdUsers[i].Password== pass) {
+            check=true;
+            alert("Login Successful. Welcome back " + regdUsers[i].Fname + "!");
+        }
+    }
+
+    if(check==false){
+        alert("Wrong credentials provided. Please try again");
+    }
+
+    form.userId.value = "";
+    form.password.value = "";
+}
+        // else if(user=="" && pass ==""){
+        
+        //     alert("Please enter valid mobile no/ email ID.")
+
+        // } 
+        // else if((regdUsers[i].Email== user || regdUsers[i].Mobile==user) && pass== ""){
+        
+        //     alert("Enter a Password")
+        // }
+        // else if((regdUsers[i].Email !== user || regdUsers[i].Mobile !== user) && 
+        //         regdUsers[i].Password !== pass){
+        //     alert("Wrong Credentials")
+        // }
+        // else if((regdUsers[i].Email==user || regdUsers[i].Mobile==user) && 
+        //         regdUsers[i].Password !== pass){
+        //     alert("Wrong Password")
+        // }
+        // else if((regdUsers[i].Email !== user || regdUsers[i].Mobile !== user) && 
+        //         regdUsers[i].Password == pass){
+        //     alert("Wrong Username/Mobile.no")
+        // }
+//     }
+//   }
